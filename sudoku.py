61e448a000dd588ee3e25fc22efd9b16918aecd7
Kirk Broadhurst <kirk.broadhurst@nielsen.com>
2017-07-13 18:09:31 -0500

    refactor game navigation stuff into module

--- sudoku.py
+++ sudoku.py
@@ -1,37 +1,11 @@
 from itertools import groupby, chain
+from navigation import *
 import numpy as np
 
 # board = np.zeros((9,9))
 board = np.loadtxt('games/003.txt')
 
 valid = [1,2,3,4,5,6,7,8,9]
-
-def places():
-	""" all the positions on a board """
-	return ((x,y) for x in range(9) for y in range(9))
-
-def squares(game, f=None):
-	""" all the positions and values on the board
-	f = group the positions according to some function of the (x,y) location (to slice rows, cols, boxes) """ 
-	result = ((p, game[p]) for p in places())
-	return result if f is None else (list(x[1]) for x in groupby(sorted(result,key=f), f))
-
-def rows(game):	
-	""" horizontal groups of squares """
-	return squares(game, lambda x: x[0][0])
-
-def cols(game):
-	""" vertical groups of squares """
-	return squares(game, lambda x: x[0][1])
-
-def boxes(game):
-	""" groups of squares in 9-block formation """
-	f = lambda x: (int(x[0][0]/3), int(x[0][1]/3))
-	return squares(game, f)
-
-def sets(game):
-	""" all the groups applicable to a game of sudoku """
-	return chain(rows(game), cols(game), boxes(game))	
 
 def find_moves(moveset):
 	""" finds any move(s) with value 0 
