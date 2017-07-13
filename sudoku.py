d4acf429b72e63f93108bee5e41938977daf0a0e
Kirk Broadhurst <kirk.broadhurst@nielsen.com>
2017-07-13 18:02:49 -0500

    improve comments

--- sudoku.py
+++ sudoku.py
@@ -11,21 +11,26 @@ def places():
 	return ((x,y) for x in range(9) for y in range(9))
 
 def squares(game, f=None):
-	""" all the positions and values on the board, optionally grouped according to some function (to slice rows, cols, boxes) """ 
+	""" all the positions and values on the board
+	f = group the positions according to some function of the (x,y) location (to slice rows, cols, boxes) """ 
 	result = ((p, game[p]) for p in places())
 	return result if f is None else (list(x[1]) for x in groupby(sorted(result,key=f), f))
 
 def rows(game):	
+	""" horizontal groups of squares """
 	return squares(game, lambda x: x[0][0])
 
 def cols(game):
+	""" vertical groups of squares """
 	return squares(game, lambda x: x[0][1])
 
 def boxes(game):
+	""" groups of squares in 9-block formation """
 	f = lambda x: (int(x[0][0]/3), int(x[0][1]/3))
 	return squares(game, f)
 
 def sets(game):
+	""" all the groups applicable to a game of sudoku """
 	return chain(rows(game), cols(game), boxes(game))	
 
 def find_moves(moveset):
