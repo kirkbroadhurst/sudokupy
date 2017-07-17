dff94c2b50eb55af9bf3c119ba54970e62632d00
Kirk Broadhurst <kirk@Kirks-MacBook-Pro.local>
2017-07-17 15:54:19 -0500

    move game tests to separate file

---
+++ test_games.py
@@ -0,0 +1,33 @@
+from sudoku import play_game, is_complete
+import numpy
+
+def test_game_1():
+    board = numpy.loadtxt('games/001.txt')
+    play_game(board)
+    assert is_complete(board)
+
+def test_game_2():
+    board = numpy.loadtxt('games/002.txt')
+    play_game(board)
+    assert is_complete(board)
+
+def test_game_3():
+    board = numpy.loadtxt('games/003.txt')
+    play_game(board)
+    assert is_complete(board)
+
+def test_game_4():
+    board = numpy.loadtxt('games/004.txt')
+    play_game(board)
+    assert is_complete(board)    
+
+def test_game_5():
+    board = numpy.loadtxt('games/005.txt')
+    play_game(board)
+    assert is_complete(board)
+
+def test_game_6():
+    board = numpy.loadtxt('games/006.txt')
+    play_game(board)
+    print (board)
+    assert is_complete(board)
--- test_sudoku.py
+++ test_sudoku.py
@@ -53,37 +53,6 @@ def test_play_single_gaps():
     play_single_gaps(board)
     assert board[(0,0)] != 0
 
-def test_game_1():
-    board = numpy.loadtxt('games/001.txt')
-    play_game(board)
-    assert is_complete(board)
-
-def test_game_2():
-    board = numpy.loadtxt('games/002.txt')
-    play_game(board)
-    assert is_complete(board)
-
-def test_game_3():
-    board = numpy.loadtxt('games/003.txt')
-    play_game(board)
-    assert is_complete(board)
-
-def test_game_4():
-    board = numpy.loadtxt('games/004.txt')
-    play_game(board)
-    assert is_complete(board)    
-
-def test_game_5():
-    board = numpy.loadtxt('games/005.txt')
-    play_game(board)
-    assert is_complete(board)
-
-def test_game_6():
-    board = numpy.loadtxt('games/006.txt')
-    play_game(board)
-    print (board)
-    assert is_complete(board)
-
 def test_make_move():
     board = numpy.loadtxt('games/001.txt')
     with pytest.raises(ValueError):
